7fcabca7b23a5a7427d02ee6b94293fca6fa9929
dltmd <dltmd@DESKTOP-F712784.mshome.net>
2023-02-04 21:59:54 +0900

    111

--- contents.js
+++ contents.js
@@ -49,7 +49,13 @@ document.addEventListener('alpine:init', () => {
         //배너 텍스트
         btext: "소 중 한 &nbsp; 당 신 을 &nbsp; 초 대 합 니 다",
         //세로 배너 텍스트
-        vtext: '예쁘게<span class="space">잘</span>살겠습니다',
+        vtext: `지금까지 곁에서 저희를 아껴주셨던
+감사한 분들을 모시고
+혼인의 예를 갖추는 것이 도리인 줄 아오나
+양가 부모님과 친지분들만 참석하여
+작은 결혼식을 올리게 되었습니다.
+넓은 마음으로 양해 부탁드리며
+멀리서나마 저희 두 사람 축복해 주시는 귀한 마음을 소중히 간직하겠습니다.`,
         //달력 요일, 시간
         calendar: 'sat.</span> <span class="time">pm 12:30',
         //남자혼주 계좌
@@ -68,7 +74,7 @@ document.addEventListener('alpine:init', () => {
         bankNum: "306-910675-39007",
         bankNumrepl: "30691067539007",
         //알림
-        alram: `<ol>
+        alram: `<ol style="padding-left: 60px; font-size: 0.550rem;">
 					<li style="list-style: decimal-leading-zero;">원할한 좌석 배치와<br>식사 준비를 위하여,참석여부와 동반인원수를 미리 말씀주시기를 부탁드립니다.</li>
 					<li style="list-style: decimal-leading-zero;">식이 종료되면,<br>앉아계신 자리로 식사가 준비됩니다.<br>스테이크 코스요리이며, 작은 뷔페가 준비되어 있습니다.</li>
 					<li style="list-style: decimal-leading-zero;">건물 내 주차공간은<br>충분하며, 3시간 무료주차입니다.</li>
@@ -78,6 +84,7 @@ document.addEventListener('alpine:init', () => {
         pt: `<dt>버스 </dt>
 			<dd>공수.당사마을 정류장 하차, 도보 3분 거리</dd>
             <dd>100번, 181번, 185번, 해운대구 마을버스 9번</dd>
+            <br>
             <dt>지하철 </dt>
 			<dd>동해선 송정역 하차, 도보 25분 거리</dd>`
     }));
