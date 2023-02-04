e87377e6390f7c02daa7594de76bc9ed73d55600
dltmd <dltmd@DESKTOP-F712784.mshome.net>
2023-02-04 22:32:29 +0900

    ㅇㅈㅂ

--- contents.js
+++ contents.js
@@ -78,7 +78,7 @@ document.addEventListener('alpine:init', () => {
 					<li style="list-style: decimal-leading-zero;">원할한 좌석 배치와 식사 준비를 위하여,<br>참석여부와 동반인원수를 미리 알려주시기를 부탁드립니다.</li>
 					<li style="list-style: decimal-leading-zero;">식이 종료되면, 앉아계신 자리로 식사가 준비됩니다.<br>스테이크 코스요리이며, 작은 뷔페가 준비되어 있습니다.</li>
 					<li style="list-style: decimal-leading-zero;">건물 내 주차공간은 충분하며,<br>3시간 무료주차입니다.</li>
-					<li style="list-style: decimal-leading-zero;">당일 단독 예식으로 오랜만에 만난 가족,<br>지인분들과 여유있는 시간을 보내시길 소망합니다.</li>
+					<li style="list-style: decimal-leading-zero;">당일 단독 예식으로, 오랜만에 만난 가족분들과<br>여유있는 시간을 보내시길 소망합니다.</li>
 					<li style="list-style: decimal-leading-zero;">식사 종료 후,식장에 장식된 꽃을 포장해드릴 예정입니다.<br>작은 꽃다발과 함께 저희 결혼식이 좋은 기억으로<br>추억되기를 바랍니다.</li>
 				</ol>`,
         pt: `<dt>버스 </dt>
